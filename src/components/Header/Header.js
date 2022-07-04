ec5a7fc73d24ee26ce6d3269fee63e946e83ff3f
emonShimoul <emonshimoul@gmail.com>
2022-07-04 13:25:26 +0600

    use NavLink in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../../images/logo.png';
 
 const Header = () => {
@@ -13,25 +13,20 @@ const Header = () => {
                     <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}c
+                        style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
                     </Nav>
                     <Nav>
-                    <Nav.Link><Link className='text-decoration-none text-dark' to="/">Home</Link></Nav.Link>
-                        <Nav.Link>
-                            <Link className='text-decoration-none text-dark' to="/donation">Donation</Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link className='text-decoration-none text-dark' to="/events">Events</Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link className='text-decoration-none text-dark' to="/blog">Blog</Link>
-                        </Nav.Link>
+                        <div className='pt-2'>
+                        <NavLink className='text-decoration-none text-dark px-3' to="/">Home</NavLink>
+                        <NavLink className='text-decoration-none text-dark px-3' to="/donation">Donation</NavLink>
+                        <NavLink className='text-decoration-none text-dark px-3' to="/events">Events</NavLink>
+                        <NavLink className='text-decoration-none text-dark px-3' to="/blog">Blog</NavLink>
+                        </div>
 
-                        <Button variant="primary mx-3">
-                            <Link className='text-decoration-none text-white' to="/register">Register</Link>
-                        </Button>
+                        <Link className='text-decoration-none text-white' to="/register"><Button variant="primary mx-3">Register</Button></Link>
+                             
                         <Button variant="secondary">Admin</Button>
                     </Nav>
                     </Navbar.Collapse>
