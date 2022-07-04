d630baee7b95435ecb66c90405db7a07d9bb14ed
emonShimoul <emonshimoul@gmail.com>
2022-07-04 11:03:47 +0600

    add basic react bootstrap navbar on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is Header</h2>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                    </Nav>
+                    <div className="d-flex me-2">
+                        <Button variant="primary me-3">Register</Button>
+                        <Button variant="secondary">Admin</Button>
+                    </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
