ee9c1b295e70f9d02cbbaa5221d9688c2f43864d
emonShimoul <emonshimoul@gmail.com>
2022-07-04 10:51:14 +0600

    set the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Donation from './components/Donation/Donation';
+import Events from './components/Events/Events';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <h2 className='text-primary'>Start Volunteer Network Project</h2>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/donation' element={<Donation />} />
+          <Route path='/events' element={<Events />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
