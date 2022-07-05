644f2fbdbb5c377de98a65b008571d4d51326fc5
emonShimoul <emonshimoul@gmail.com>
2022-07-05 12:05:50 +0600

    remove unnecessary css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,41 +6,41 @@ const Home = () => {
         <div className='mt-5'>
             <div className="container">
                 <h2>I GROW BY HELPING PEOPLE IN NEED.</h2>
-                <div class="input-group mb-3 w-25 mx-auto mt-4">
-                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                    <button class="btn btn-primary" type="button" id="button-addon2">Search</button>
+                <div className="input-group mb-3 w-25 mx-auto mt-4">
+                    <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button className="btn btn-primary" type="button" id="button-addon2">Search</button>
                 </div>
 
-                <div class="row row-cols-1 row-cols-md-4 g-4">
-                    <div class="col">
-                        <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                <div className="row row-cols-1 row-cols-md-4 g-4">
+                    <div className="col">
+                        <div className="card event-card">
+                        <img src="https://i.ibb.co/34bDjLB/animal-Shelter.png" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
                         </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://i.ibb.co/Lv9HgmP/babySit.png" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
                         </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://i.ibb.co/8j3BVwX/bird-House.png" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
                         </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://i.ibb.co/ZdrCTSz/child-Support.png" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
                         </div>
                         </div>
                     </div>
