cd84ec26ee7506b635988f18f238cf2ef63d7691
emonShimoul <emonshimoul@gmail.com>
2022-07-05 10:35:33 +0600

    add searchbar in the homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className='mt-5'>
+            <h2>I GROW BY HELPING PEOPLE IN NEED.</h2>
+            <div class="input-group mb-3 w-25 mx-auto mt-4">
+                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                <button class="btn btn-primary" type="button" id="button-addon2">Search</button>
+            </div>
         </div>
     );
 };
