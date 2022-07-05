533f90bd66ec1a8c409d8be627c7e82f27fd8be4
emonShimoul <emonshimoul@gmail.com>
2022-07-06 01:55:46 +0600

    complete register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import logo from '../../images/logo.png';
+import './Register.css';
 
 const Register = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("firstName")} placeholder="Name" />
-                <input {...register("lastName")} placeholder="Last Name" />
-                <input type="number" {...register("age")} placeholder="Age" />
-                <input type="submit" />
-            </form>
+        <div className='container my-5'>
+            <img src={logo} alt="" width="200px" className='mb-5' />
+            <div className="register-form">
+                <h4 className='mb-4 fw-bold text-start'>Register as a Volunteer</h4>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("firstName")} type="text" placeholder="Full Name" className='input-group input-field' />
+                    <input {...register("lastName")} type="email" placeholder="Email" className='input-group input-field' />
+                    <input {...register("date")} type="date" placeholder="Date" className='input-group input-field' />
+                    <input {...register("description")} type="text" placeholder="Description" className='input-group input-field' />
+                    <input {...register("event")} type="text" placeholder="Event Name" className='input-group input-field' />
+                    <input type="submit" className='btn btn-primary mt-4' />
+                </form>
+            </div>
         </div>
     );
 };
