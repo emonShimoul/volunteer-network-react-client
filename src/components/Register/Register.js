0a68be245f1bb250bf1bb313ae3c8847127f54f1
emonShimoul <emonshimoul@gmail.com>
2022-07-06 01:22:47 +0600

    added react hook form successfully

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div>
-            <h2>This is Register</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("firstName")} placeholder="Name" />
+                <input {...register("lastName")} placeholder="Last Name" />
+                <input type="number" {...register("age")} placeholder="Age" />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
