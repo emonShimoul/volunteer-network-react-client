0e4303ebe4c330a30555f2dabd033551a309cb86
emonShimoul <emonshimoul@gmail.com>
2022-07-10 15:21:00 +0600

    display all events in events page

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const Events = () => {
+    const[events, setEvents] = useState([]);
+    const {user} = useAuth();
+    // console.log(user);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/events')
+        .then(res => res.json())
+        .then(data => setEvents(data));
+    })
     return (
         <div>
             <h2>This is Events</h2>
+            {
+                events.map(event => <li key= {event._id}>
+                    {event.name} - {event.event}
+                </li> )
+            }
         </div>
     );
 };
