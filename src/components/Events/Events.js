33d714116e8a50a83e547b3871eed9cbb19a8065
emonShimoul <emonshimoul@gmail.com>
2022-07-10 23:09:49 +0600

    use query parameter to send specific users mail to load data

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -4,13 +4,19 @@ import useAuth from '../../hooks/useAuth';
 const Events = () => {
     const[events, setEvents] = useState([]);
     const {user} = useAuth();
-    // console.log(user);
+    console.log(user.email);
 
     useEffect(() => {
-        fetch('http://localhost:5000/events')
+        fetch(`http://localhost:5000/events/${user.email}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            params: {
+                user: {user}
+            }
+        })
         .then(res => res.json())
         .then(data => setEvents(data));
-    })
+    }, []);
     return (
         <div>
             <h2>This is Events</h2>
