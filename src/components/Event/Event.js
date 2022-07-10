7abb6db2de195dfd0b98913234747594c8b1c10c
emonShimoul <emonshimoul@gmail.com>
2022-07-11 01:10:47 +0600

    Delete api to delete a specific event

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const Event = (props) => {
     const userData = props.userData;
-    console.log(userData);
+    // console.log(userData._id);
+    const handleRemoveEvents = (id) => {
+        fetch(`http://localhost:5000/events/${id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        })
+    }
     return (
         <div className='bg-secondary text-white m-4 py-4 rounded-3'>
             <h5>{userData.event}</h5>
             <p>{userData.date}</p>
-            <button className='btn btn-warning'>Remove</button>
+            <button className='btn btn-warning' onClick={() => handleRemoveEvents(userData._id)}>Remove</button>
         </div>
     );
 };
