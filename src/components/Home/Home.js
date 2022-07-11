b166c967c809a6beaf0cc7b7c68f62877618c0e5
emonShimoul <emonshimoul@gmail.com>
2022-07-11 13:37:37 +0600

    display all events in the home page from database

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './Home.css';
 
 const Home = () => {
+    const [allEvents, setAllEvents] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/events`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(res => res.json())
+        .then(data => setAllEvents(data));
+    }, []);
+    console.log(allEvents);
     return (
         <div className='mt-5'>
             <div className="container">
@@ -12,38 +23,20 @@ const Home = () => {
                 </div>
 
                 <div className="row row-cols-1 row-cols-md-4 g-4 mt-4">
-                    <div className="col">
-                        <div className="card event-card">
-                            <img src="https://i.ibb.co/34bDjLB/animal-Shelter.png" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title text-white">Card title</h5>
+                {
+                    allEvents.map(event => 
+                        
+                        <div className="col">
+                            <div className="card event-card">
+                                <img src="https://i.ibb.co/34bDjLB/animal-Shelter.png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title text-white">{event.event}</h5>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col">
-                        <div className="card event-card">
-                            <img src="https://i.ibb.co/Lv9HgmP/babySit.png" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title text-white">Card title</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card event-card">
-                            <img src="https://i.ibb.co/8j3BVwX/bird-House.png" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title text-white">Card title</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card event-card">
-                            <img src="https://i.ibb.co/ZdrCTSz/child-Support.png" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title text-white">Card title</h5>
-                            </div>
-                        </div>
-                    </div>
+                    
+                    )
+                }
                 </div>
             </div>
         </div>
